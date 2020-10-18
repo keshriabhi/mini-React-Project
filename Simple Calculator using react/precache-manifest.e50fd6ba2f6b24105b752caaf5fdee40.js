b0534230b1e7ed3cb9df99caf47c076de09ba612
Abhishek keshri <abhishekkeshri@live.com>
2020-10-18 19:17:03 +0530

    Add files via upload

---
+++ Simple Calculator using react/precache-manifest.e50fd6ba2f6b24105b752caaf5fdee40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "145515e14a3423c4c317f3424bbd8382",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e3eb7467dc1f453e821",
+    "url": "/static/css/main.5f7ebbd0.chunk.css"
+  },
+  {
+    "revision": "957638ea8982320dc693",
+    "url": "/static/js/2.e3675be0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e3675be0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e3eb7467dc1f453e821",
+    "url": "/static/js/main.998175a8.chunk.js"
+  },
+  {
+    "revision": "76e0429abd4676778da5",
+    "url": "/static/js/runtime-main.61436ac2.js"
+  }
+]);
